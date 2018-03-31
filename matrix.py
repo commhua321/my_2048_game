18c73c3bd4933691745825de4b7e9ef6c8c20d79
commhua321 <commhua321@gmail.com>
2018-03-31 22:20:11 +0800

    game

--- matrix.py
+++
@@ -1,7 +0,0 @@
-
-class UpdateMatrix():
-    def __init__(self, ai_settings):
-        super(UpdateMatrix, self).__init__()
-        self.matrix = ai_settings.matrix
-        self.score = 0
-        self.zerolist = []
